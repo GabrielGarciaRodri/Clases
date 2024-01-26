1d0da512a45e791f410c6cf1b91ab57a6dd1f29d
unknown <GabrielGarciaRodri>
2024-01-25 23:36:12 -0500

    algoritmos

--- 25enero/ecuacion_primer_grado.py
+++ 25enero/ecuacion_primer_grado.py
@@ -4,17 +4,18 @@ os.system('cls')
 a = float(input("Ingrese a: "))
 b = float(input("Ingrese b: "))
 
-def resolver_ecuacion_lineal(a, b):
-    # Verificar si 'a' es diferente de cero para evitar divisiones por cero
+def respuesta(a, b):
+
     if a != 0:
-        # Despejar 'x' de la ecuación ax + b = 0
-        x = -b / a
-        return x
+        x = -b/a
+        return print(f"solucion unica: {x}")
+    
+    elif a == 0 and b == 0:
+        return "No hay solucion unica"
+    
     else:
-        # La ecuación no tiene solución si 'a' es igual a cero
-        return "La ecuación no tiene solución real cuando 'a' es igual a cero."
+        return "Sin solucion"
     
-x = -b/a
-solucion = resolver_ecuacion_lineal(a, b)
+solucion = respuesta(a, b)
     
-print(f"La solución de {a}x + {b} = 0 es x = {solucion}")
+print(solucion)
---
+++ 25enero/media_armonica.py
@@ -0,0 +1,18 @@
+import os
+os.system('cls')
+
+num = int(input("¿Cuantos numeros va a ingresar?) "))
+
+lista = []
+
+for i in range (1, num + 1):
+    numero = float (input(f"ingrese el numero {i}: "))
+    #almacena el numero i dentro de la lista con append
+    lista.append(numero)
+
+#En cada iteración, la variable numero toma el valor del siguiente elemento en la lista.
+media = (num / sum(1/numero for numero in lista))
+
+print (f"Numeros ingresados: {lista}")
+
+print (f"La media es: {media}")
