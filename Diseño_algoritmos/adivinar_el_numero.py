9c6cbc35db72a6e170ba64127b0a2c970b5478ec
GabrielGarciaRodri <145493679+GabrielGarciaRodri@users.noreply.github.com>
2024-01-29 01:54:53 -0500

    fix: :art: File adivinar_el_numero.py created

--- Diseño_algoritmos/adivinar_el_numero.py
+++ Diseño_algoritmos/adivinar_el_numero.py
@@ -1,7 +1,7 @@
 import os
 os.system('cls')
 
-from random import randrange
+"""from random import randrange
 n = randrange(101)
 
 intentos = 0
@@ -17,5 +17,31 @@ while True:
         print(f"Es mayor que {intento}")
     else:
         intento > n
-        print(f"Es menor que {intento}")
+        print(f"Es menor que {intento}")"""
+
+#Ahora la num_comp adivina
+import random
+
+num = int(input("Piensa en un número entre 1 y 100. Ingrésalo aquí: "))
+    
+lim_inf = 1
+lim_sup = 100
+intentos = 0
+    
+while True:
+    num_comp = random.randint(lim_inf, lim_sup)
+    print(f"¿Es {num_comp} tu número?")
+        
+    respuesta = input("Ingresa '<', '>', o '=': ")
+    intentos += 1
+        
+    if respuesta == "=":
+        print(f"¡Adivine en {intentos} intentos B-)")
+        break
+    elif respuesta == "<":
+        lim_sup = num_comp - 1
+    elif respuesta == ">":
+        lim_inf = num_comp + 1
+    else:
+        print("Por favor, ingresa '<', '>', o '='.")
         
