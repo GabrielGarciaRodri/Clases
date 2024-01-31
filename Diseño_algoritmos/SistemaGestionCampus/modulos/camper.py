5ad845a720d30fdad1b065c6c89d8b30ee408099
GabrielGarciaRodri <145493679+GabrielGarciaRodri@users.noreply.github.com>
2024-01-31 15:49:34 -0500

    refactor: :construction: function delete created

--- Diseño_algoritmos/SistemaGestionCampus/modulos/camper.py
+++ Diseño_algoritmos/SistemaGestionCampus/modulos/camper.py
@@ -1,53 +1,114 @@
 #Guarda la funcionalidad del CRUD
 import os
 from tabulate import tabulate
-from .variables import save, getAll
+from .variables import save, getAll, camper
 
 def create():
     
-    os.system("clear")
+    os.system("cls")
     print("""
           *********************************
           *     FORMULARIO DEL CAMPER     *
           *********************************
           """)
     save({
-        "nombre" :input("Ingrese el nombre del camper: "),
-        "apellido" : input("Ingrese el apellido del camper: "),
-        "edad" : int(input("Ingrese la edad del camper: "))
+        "Nombre" :input("Ingrese el nombre del camper: "),
+        "Apellido" : input("Ingrese el apellido del camper: "),
+        "Edad" : int(input("Ingrese la edad del camper: "))
     })
     os.system('pause')
     
-def read():
-    print("""
+def read(codigo=None):
+    os.system('cls')
+    
+    if(not codigo != None):
+        print(f"""
              *********************************
              *        TABLA DEL CAMPER       *
              ********************************* 
             """)
-    print(tabulate(getAll(),headers=["Nombres","Apellidos","Edad"]))
+
+        for i, val in enumerate (getAll()):
+            print(f"""
+            __________________________________
+            Codigo: {i+1}  
+            Nombre: {val.get("Nombre")}
+            Apellido: {val.get("Apellido")}
+            Edad: {val.get("Edad")}
+            __________________________________
+            """)
+    else:
+        #codigo -1 se utiliza para retroceder una posicion en la lista es decir 0-1
+        val = getAll()[codigo-1]
+        print(f"""
+            __________________________________
+            Codigo: {codigo}  
+            Nombre: {val.get("Nombre")}
+            Apellido: {val.get("Apellido")}
+            Edad: {val.get("Edad")}
+            __________________________________
+            """)
     os.system('pause')
     
 def update():
     print("El camper se actualizó")
 
 def delete():
-    print("El camper se borró")
+    
+    bandera = True
+    while (bandera):
+        os.system('cls')
+        read()
+        print("""
+        *********************************
+        *        ELIMINAR CAMPER        *
+        ********************************* 
+            """)
+        codigo = int(input(f"Ingrese el codigo del camper que desea borrar:\n "))
+        read(codigo)
+        print("""
+            ¿Está seguro que desea eliminar al camper?
+            1. Si
+            2. No
+            3. Cancelar
+            """)
+        opc = int(input())
+        match(opc):
+                #Con getAll toma el array y con pop elimina el(codigo)
+            case 1: 
+                val = camper.pop(codigo-1)
+                os.system('cls')
+                print(f"""
+                    Camper eliminado:
+                    __________________________________
+                    Codigo: {codigo}  
+                    Nombre: {val.get("Nombre")}
+                    Apellido: {val.get("Apellido")}                            
+                    Edad: {val.get("Edad")}
+                    __________________________________
+                    """)
+                os.system("pause")
+                bandera = False
+                
+            case 3:
+                bandera = False
     
 def menu():
     
-    menu = ["Crear","Buscar","actualizar","Borrar","Salir"]
+    
     """El siguiente try permite repetir la opcion en caso de que
     El usuario se equivoque"""
     while True:
         #Enumera el array menu
-        os.system("clear")
+        os.system("cls")
         print("""
              *********************************
              *        MENU DEL CAMPER        *
              ********************************* 
             """)
+        menu = ["Crear","Buscar","actualizar","Borrar","Salir"]
+
         print("".join([f"{i+1}. {val}\n" for i, val in enumerate(menu)]))
-        
         try: 
             opc = int(input())
             if(opc<=len(menu) and opc>0):
