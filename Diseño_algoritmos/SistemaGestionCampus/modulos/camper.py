7d865b0ff69d4f777475669d2e61b31a9756c28c
GabrielGarciaRodri <145493679+GabrielGarciaRodri@users.noreply.github.com>
2024-01-30 22:05:49 -0500

    feat: :tada: Project CRUD created
    File SistemaGestionCampus is a CRUD used like a system of administration of campers

---
+++ Diseño_algoritmos/SistemaGestionCampus/modulos/camper.py
@@ -0,0 +1,68 @@
+#Guarda la funcionalidad del CRUD
+import os
+
+from .variables import save, getAll
+
+def create():
+    
+    os.system("clear")
+    print("""
+          *********************************
+          *     FORMULARIO DEL CAMPER     *
+          *********************************
+          """)
+    save({
+        "nombre" :input("Ingrese el nombre del camper: "),
+        "apellido" : input("Ingrese el apellido del camper: "),
+        "edad" : int(input("Ingrese la edad del camper: "))
+    })
+    os.system('pause')
+    
+def read():
+    print("""
+             *********************************
+             *        TABLA DEL CAMPER       *
+             ********************************* 
+            """)
+    os.system('cls')
+    plantilla = ""
+    for i, val in enumerate(getAll()):
+        
+        print(getAll())
+        os.system('pause')
+    
+def update():
+    print("El camper se actualizó")
+
+def delete():
+    print("El camper se borró")
+    
+def menu():
+    
+    menu = ["Crear","Buscar","actualizar","Borrar","Salir"]
+    """El siguiente try permite repetir la opcion en caso de que
+    El usuario se equivoque"""
+    while True:
+        #Enumera el array menu
+        os.system("clear")
+        print("""
+             *********************************
+             *        MENU DEL CAMPER        *
+             ********************************* 
+            """)
+        print("".join([f"{i+1}. {val}\n" for i, val in enumerate(menu)]))
+        
+        try: 
+            opc = int(input())
+            if(opc<=len(menu) and opc>0):
+                match(opc):
+                    case 1:create()
+                    case 2:read()
+                    case 3:update()
+                    case 4:delete()
+                    case 5:
+                        print("Gracias por usar<3")
+                        break
+                
+        except ValueError:
+            print(f"Opcion no habilitada")
